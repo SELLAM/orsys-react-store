a1cf35b27fb99c1ed255abfd78ddd30e84c7cbc6
SELLAM <msellam90@gmail.com>
2019-10-22 18:11:40 +0100

    fix

--- ReactStore.jsx
+++ ReactStore.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 export function ReactStore() {
     const RStore = new Store()
 
-    RStore.connect = function (subject, WrappedComponent) {
+    RStore.connect = (subject, WrappedComponent) => {
         return class extends Component {
             constructor(props) {
                 super(props);
